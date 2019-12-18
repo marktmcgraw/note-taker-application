65a1662d482e74db1b05a4ba77565bfe7f5915da
Cotton Top <mtmcgr@gmail.com>
2019-12-17 21:24:55 -0600

    troubleshooting pathways and get methods

--- server.js
+++ server.js
@@ -2,8 +2,9 @@
 // =============================================================
 const express = require("express");
 const path = require("path");
+const fs = require("fs");
 
-app.use(express.static("public"));
+
 
 // Sets up the Express App
 // =============================================================
@@ -14,15 +15,17 @@ const PORT = process.env.PORT || 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+app.use(express.static("public"));
+
 
 // Routes
 // =============================================================
 // Basic route that sends the user first to the AJAX Page
 app.get("/", function(req, res) {
-  res.sendFile(path.join(__dirname, "index.html"));
+  res.sendFile(path.join(__dirname, "public/assets/index.html"));
 });
 app.get("/notes", function(req, res) {
-  res.sendFile(path.join(__dirname, "notes.html"));
+  res.sendFile(path.join(__dirname, "public/assets/notes.html"));
 });
 
 // Displays all notes
